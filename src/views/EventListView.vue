6e9b8f2810b6a0bfda0ee9c38c05d0e95dec0e25
Yotsawat <yotsawat_l@cmu.ac.th>
2023-08-06 22:53:21 +0700

    ...

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,96 +1,93 @@
 <script setup lang="ts">
 import EventCard from '../components/EventCard.vue'
 import type { EventItem } from '@/type'
-import { computed, ref, watchEffect } from 'vue'
-import { onBeforeRouteUpdate } from 'vue-router'
+import { ref, watchEffect, type Ref, computed } from 'vue'
 import EventService from '@/services/EventService'
-import type { AxiosResponse } from 'axios'
+import type { AxiosResponse } from 'axios';
 import { useRouter } from 'vue-router'
 import NProgress from 'nprogress'
+import { onBeforeRouteUpdate } from 'vue-router';
 
-const events = ref<Array<EventItem>>([])
+const events: Ref<Array<EventItem>> = ref([])
+
+const router = useRouter()
+
 const totalEvent = ref<number>(0)
+
 const props = defineProps({
   page: {
     type: Number,
     required: true
-  },
+  }
+  ,
   limit: {
     type: Number,
     required: true
   }
 })
 
-const router = useRouter()
 // eslint-disable-next-line vue/no-setup-props-destructure
 const limit = ref(props.limit)
 
-const hasNextPage = computed(() => {
-  //First calculate the total page
-  const totalPages = Math.ceil(totalEvent.value / 2)
-  return props.page.valueOf() < totalPages
-})
-
 const increaseLimit = () => {
-  limit.value++
+  limit.value++;
   router.push({ name: 'event-list', query: { page: props.page, limit: limit.value } })
 }
+
 const decreaseLimit = () => {
   if (limit.value > 1) {
-    limit.value--
+    limit.value--;
     router.push({ name: 'event-list', query: { page: props.page, limit: limit.value } })
   }
 }
 
 
-  // eslint-disable-next-line vue/no-setup-props-destructure
-  EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
-    events.value = response.data
-    totalEvent.value = response.headers['x-total-count']
-  }).catch(()=>{
-    router.push({ name: 'NetworkError'})
-  })
+// eslint-disable-next-line vue/no-setup-props-destructure
+EventService.getEvent(3, props.page).then((response: AxiosResponse<EventItem[]>) => {
+  events.value = response.data
+  totalEvent.value = response.headers['x-total-count']
+}).catch(() => {
+  router.push({ name: 'NetworkError' })
+})
 
 onBeforeRouteUpdate((to, from, next) => {
-    const toPage = Number(to.query.page)
-    EventService.getEvent(2, props.page).then((response: AxiosResponse<EventItem[]>) => {
+  const toPage = Number(to.query.page)
+  EventService.getEvent(3, toPage).then((response: AxiosResponse<EventItem[]>) => {
     events.value = response.data
     totalEvent.value = response.headers['x-total-count']
-  }).catch(()=>{
-    router.push({ name: 'NetworkError'})
+    next()
+  }).catch(() => {
+    next({ name: 'NetworkError' })
   })
-  })
+})
 
-</script>
+const hasNextPage = computed(() => {
+  const totalPages = Math.ceil(totalEvent.value / 3)
+  return props.page.valueOf() < totalPages
+})
+
+</script> 
 
 <template>
-  <h1>
-    Events For Good
+  <h1>Events For Good 
     <button @click="increaseLimit">+</button>
     <button @click="decreaseLimit">-</button>
     {{ limit }}
   </h1>
+  
   <main class="events">
     <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
-
     <div class="pagination">
-      <RouterLink
-        :to="{ name: 'event-list', query: { page: page - 1, limit: limit } }"
-        rel="prev"
-        v-if="page != 1"
-        id="page-prev"
-      >
-        Prev Page
+      <RouterLink :to="{ name: 'event-list', query: { page: page - 1, limit: limit } }" rel="prev" v-if="page != 1"
+        id="page-prev"> Prev
+        Page
       </RouterLink>
-      <RouterLink
-        :to="{ name: 'event-list', query: { page: page + 1, limit: limit } }"
-        rel="next"
-        v-if="hasNextPage"
-        id="page-next"
-      >
+      <RouterLink :to="{ name: 'event-list', query: { page: page + 1, limit: limit } }" rel="next" v-if="hasNextPage"
+        id="page-next">
         Next Page
       </RouterLink>
     </div>
+
   </main>
 </template>
 
@@ -100,9 +97,16 @@ onBeforeRouteUpdate((to, from, next) => {
   flex-direction: column;
   align-items: center;
 }
+
 .pagination {
   display: flex;
   width: 290px;
+}
+
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
 }
 
 #page-prev {
